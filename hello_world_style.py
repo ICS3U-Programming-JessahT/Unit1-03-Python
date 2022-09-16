f7d98add916f74fdc5f834d648787d40273e5007
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2022-09-16 06:16:17 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Name: Jessah
+# 09/16/2022
+# This program displays Hello world
+
+
+
+def main():
+  print("Hello world, but with style")
+
+
+if __name__ == "__main__":
+    main()
